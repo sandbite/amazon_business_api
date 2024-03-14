63490a88b5beac1a252524aec8e297d239ae67c6
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-03-14 14:53:41 +0200

    wip: reporting resources

---
+++ lib/amazon_business_api/reporting/resources/money.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module AmazonBusinessApi
+  module Reporting
+    class Money < AmazonBusinessApi::Resource
+      # https://developer-docs.amazon.com/amazon-business/docs/reporting-api-v1-reference-1
+      attribute :currencyCode, type: LedgerSync::Type::String
+      attribute :amount, type: LedgerSync::Type::String
+    end
+  end
+end
