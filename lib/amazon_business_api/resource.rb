25283077f088003a955d97af374b14894b8f731a
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-03-14 17:41:34 +0200

    feat: add serialize helper method to resource

--- lib/amazon_business_api/resource.rb
+++ lib/amazon_business_api/resource.rb
@@ -2,5 +2,12 @@
 
 module AmazonBusinessApi
   class Resource < LedgerSync::Resource
+    def serialize
+      result = {}
+      to_h.each_key do |key|
+        result[key] = to_h[key].try(:to_h) || to_h[key]
+      end
+      result
+    end
   end
 end
