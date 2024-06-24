6cb111506f3be0a7814db7af9e87c5c1d30f8c32
Jean-Michel Keuleyan <jm.keuleyan@gmail.com>
2024-06-24 14:11:45 +0200

    WIP FIND OP

---
+++ lib/amazon_business_api/place_order/operations/find.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module AmazonBusinessApi
+  class PlaceOrder
+    module Operations
+      class Find < AmazonBusinessApi::Operation::Find
+        class Contract < LedgerSync::Ledgers::Contract
+          params do
+            required(:external_id).filled(:string)
+          end
+        end
+
+        private
+
+        def hash_to_deserialize
+          response.body
+        end
+
+        def url
+          "/ordering/2022-10-30/orders/#{resource.external_id}"
+        end
+
+        def opts
+          {}
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/amazon_business_api/place_order/serializer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module AmazonBusinessApi
+  class PlaceOrder
+    class Serializer < AmazonBusinessApi::Serializer
+      attribute :externalId
+    end
+  end
+end
