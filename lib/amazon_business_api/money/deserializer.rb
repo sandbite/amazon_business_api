d61a19114b83c1db783b1b09da6ca9bc4899d268
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-06-28 15:55:56 +0300

    fix: ensure that Money.amount is string
    it behaves differently in Ordering API

--- lib/amazon_business_api/money/deserializer.rb
+++ lib/amazon_business_api/money/deserializer.rb
@@ -4,7 +4,10 @@ module AmazonBusinessApi
   class Money
     class Deserializer < AmazonBusinessApi::Deserializer
       attribute :currency_code, hash_attribute: :currencyCode
-      attribute :amount
+      attribute :amount do |response|
+        # Reporting API returns string and Ordering API returns float
+        response.dig(:hash, 'amount').try(:to_s)
+      end
     end
   end
 end
