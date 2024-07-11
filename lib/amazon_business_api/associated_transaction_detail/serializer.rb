76bd163213ba19734e36a5fd2515c2db055aa835
Jean-Michel Keuleyan <jm.keuleyan@gmail.com>
2024-07-11 16:09:10 +0200

    Remove GET endpoint from PR

--- lib/amazon_business_api/associated_transaction_detail/serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AmazonBusinessApi
-  class AssociatedTransactionDetail
-    class Serializer < AmazonBusinessApi::Serializer
-      attribute :associatedTransactionId, resource_attribute: :associated_transaction_id
-    end
-  end
-end
--- lib/amazon_business_api/resources/associated_transaction_detail.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module AmazonBusinessApi
-  class AssociatedTransactionDetail < AmazonBusinessApi::Resource
-    # https://developer-docs.amazon.com/amazon-business/docs/reconciliation-api-v1-reference
-    attribute :associated_transaction_id, type: LedgerSync::Type::String
-  end
-end
