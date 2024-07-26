2c0b7b910f9069400ed645014b7d6fb30d086506
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-07-26 15:42:41 +0300

    fix: add missing require

--- lib/amazon_business_api/search_products_by_keyword/deserializer.rb
+++ lib/amazon_business_api/search_products_by_keyword/deserializer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../search_product_result/deserializer'
+
 module AmazonBusinessApi
   class SearchProductsByKeyword
     class Deserializer < AmazonBusinessApi::Deserializer
