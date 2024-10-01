753919d52a86da93195c486cbc4e44359c8122d7
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-10-01 11:20:35 +0300

    feat: add external_id to result line item deserializer

--- lib/amazon_business_api/result_line_item/deserializer.rb
+++ lib/amazon_business_api/result_line_item/deserializer.rb
@@ -6,6 +6,7 @@ require_relative '../rejected_item/deserializer'
 module AmazonBusinessApi
   class ResultLineItem
     class Deserializer < AmazonBusinessApi::Deserializer
+      attribute :external_id, hash_attribute: :externalId
       references_many :accepted_items, deserializer: AcceptedItem::Deserializer, hash_attribute: :acceptedItems
       references_many :rejected_items, deserializer: RejectedItem::Deserializer, hash_attribute: :rejectedItems
     end
