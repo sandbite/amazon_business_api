0850a175a81e4b7205f55133d54e42917241e7a0
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-10-02 16:07:00 +0300

    fix: place order expectation amount

--- lib/amazon_business_api/resources/place_order_expectation.rb
+++ lib/amazon_business_api/resources/place_order_expectation.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'place_order_expectations/amount'
+require_relative '../money'
 
 module AmazonBusinessApi
   class PlaceOrderExpectation < AmazonBusinessApi::Resource
     # https://developer-docs.amazon.com/amazon-business/docs/ordering-api-v1-reference
     attribute :expectation_type, type: LedgerSync::Type::String
-    references_one :amount, to: Amount
+    references_one :amount, to: Money
     attribute :source, type: LedgerSync::Type::String
   end
 end
